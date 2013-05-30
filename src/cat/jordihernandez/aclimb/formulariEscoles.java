e23f4522614c3defeab02e7124ef86acb06d019e
jordhi <jordhi@gmail.com>
2013-05-30 17:52:24 +0200

    formulari escoles update insert

--- src/cat/jordihernandez/aclimb/formulariEscoles.java
+++ src/cat/jordihernandez/aclimb/formulariEscoles.java
@@ -2,7 +2,10 @@ package cat.jordihernandez.aclimb;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class formulariEscoles extends Activity {
 	EditText nomescola, comentaris;
@@ -12,7 +15,31 @@ public class formulariEscoles extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.form_escoles);
+		
+		nomescola = (EditText)this.findViewById(R.id.etxtNomEscola);
+		comentaris = (EditText)this.findViewById(R.id.etxComentaris);
+		
 	}
 	 
-
+	public void InserirEscola (View view) {
+		
+		manipularDadesEscoles descoles = new manipularDadesEscoles(this);
+		
+		descoles.obrir();
+		// Agafar les dades dels widgets
+		item_escoles escola_nova = new item_escoles(nomescola.getText().toString(),comentaris.getText().toString());
+		
+		
+		// Inserir a la base de dades i tancar
+		descoles.inserirEscola(escola_nova);
+		descoles.tancar();
+		
+		finish();
+		
+	}
+	
+	public void CancelarNovaEscola (View view) {
+		finish();
+		
+	}
 }
