1f920451e279e40089179868298347dc39a8c75c
jordhi <jordhi@gmail.com>
2013-06-03 16:27:22 +0200

    spinner escoles afegit form vies

--- src/cat/jordihernandez/aclimb/formulariVies.java
+++ src/cat/jordihernandez/aclimb/formulariVies.java
@@ -1,5 +1,8 @@
 package cat.jordihernandez.aclimb;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +18,7 @@ import android.widget.TextView;
 
 public class formulariVies extends Activity {
 	EditText txtvia,txtgrau,txtDescens;
-	Spinner spOrientacio;
+	Spinner spOrientacio, spEscoles, spSectors;
 	RadioGroup optTipus;
 	RadioButton tipus,tipusEsp,tipusClas;
 	TextView lblDescens;
@@ -28,17 +31,39 @@ public class formulariVies extends Activity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.form_vies);
-				
+		
+		manipularDadesEscoles descoles = new manipularDadesEscoles(this);
+		//Arrays per agafar llistat d'escoles i posar-los al spinner d'escoles
+		ArrayList<item_escoles> llista_escoles = new ArrayList<item_escoles>();
+		ArrayList<String> nom_escoles = new ArrayList<String>();
+		//item_escoles per sel.leccionar una escola quan ens recorrem l'Array
+		item_escoles item = new item_escoles();
 		
 		txtgrau = (EditText)this.findViewById(R.id.txtGrau);
 		rating = (RatingBar)this.findViewById(R.id.ratQualitat);
 		TopRope = (CheckBox)this.findViewById(R.id.chkToprope);
 		
-		//disseny del spinner
+		//disseny del spinner Orientacio
 		spOrientacio = (Spinner)this.findViewById(R.id.cmbOrientacio);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_orientacio, android.R.layout.simple_spinner_item);
 		spOrientacio.setAdapter(adapter);
-				
+		
+		//Omplir spinner Escoles
+		spEscoles = (Spinner)this.findViewById(R.id.frmViescmbEscola);
+		descoles.obrir();
+		llista_escoles = descoles.getAllEscoles();
+		ListIterator<item_escoles> it = llista_escoles.listIterator();
+		//Recórrer escoles i omplir array<string> amb el nom de les escoles
+		while(it.hasNext()) {
+			item = (item_escoles)it.next();
+			nom_escoles.add(item.getNomEscola());
+		}
+		//adaptar l'spinner a l'array dels noms escoles
+		ArrayAdapter<String> adapter_escoles = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, nom_escoles);
+		adapter_escoles.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spEscoles.setAdapter(adapter_escoles);
+		descoles.tancar();
+		
 		txtDescens = (EditText)this.findViewById(R.id.txtDescens);
 		lblDescens = (TextView)this.findViewById(R.id.lblDescens);
 		
